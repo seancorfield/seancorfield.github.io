e6031be3fa12be297a2186d480ba63dfa4e537e9
Sean Corfield <sean@corfield.org>
2020-10-28 12:10:56 -0700

    Allow sitemap to exclude paths

---
+++ src/cryogen_core/sitemap.clj
@@ -0,0 +1,28 @@
+(ns cryogen-core.sitemap
+  (:require [clojure.xml :refer [emit]]
+            [cryogen-core.io :as cryogen-io])
+  (:import java.util.Date))
+
+;;generate sitemaps using the sitemap spec
+;;http://www.sitemaps.org/protocol.html
+
+(defn format-date [date]
+  (let [fmt (java.text.SimpleDateFormat. "yyyy-MM-dd")]
+    (.format fmt date)))
+
+(defn loc [^java.io.File f]
+  (-> f (.getAbsolutePath) (.split (cryogen-io/path cryogen-io/public "/")) second))
+
+(defn generate [site-url ignored-files ignored-paths]
+  (with-out-str
+    (emit
+      {:tag :urlset
+       :attrs {:xmlns "http://www.sitemaps.org/schemas/sitemap/0.9"}
+       :content
+       (for [^java.io.File f (cryogen-io/find-assets cryogen-io/public #{".html"} ignored-files ignored-paths)]
+         {:tag :url
+          :content
+          [{:tag :loc
+            :content [(str site-url (loc f))]}
+           {:tag :lastmod
+            :content [(-> f (.lastModified) (Date.) format-date)]}]})})))
